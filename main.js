e33af62a424d09bf0f66c8fefb5870f0e3dc65e0
javascriptdeveloper77 <mnh.webnurhuda@gmail.com>
2021-12-30 15:23:03 +0600

    add break js statement

--- main.js
+++ main.js
@@ -1,2 +1,11 @@
 "use strict";
-console.log("Hello");
+let start = 1;
+while (start <= 10) {
+  if (start == 5) {
+    break;
+  }
+  console.log("Hello " + start);
+  start++;
+}
+
+// increament 10 porjonto run exeecute hobe; but break dile break er oi number er aghe increment stop hoye jabe;

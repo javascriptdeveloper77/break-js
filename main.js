7e3f6beb39de062068de3a1a47ef68bb649748b9
javascriptdeveloper77 <mnh.webnurhuda@gmail.com>
2021-12-30 16:02:01 +0600

    add break with prompt value

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 "use strict";
+/*
 let start = 1;
 while (start <= 10) {
   if (start == 5) {
@@ -7,6 +8,7 @@ while (start <= 10) {
   console.log("Hello " + start);
   start++;
 }
+*/
 
 // increament 10 porjonto run exeecute hobe; but break dile break er oi number er aghe increment stop hoye jabe;
 /*
@@ -14,6 +16,50 @@ while (start <= 10) {
     Hello 2
     Hello 3
     Hello 4
-    Hello 5
-
 */
+
+// true value
+/*
+{
+  let start = 1;
+
+  while (true) {
+    // console.log("Hello " + start ); 6 count hobe
+    if (start == 6) {
+      break;
+    }
+    console.log("Hello " + start); // 6 count hobena
+    start++;
+  }
+}
+*/
+
+// break with prompt
+/*
+{
+  let myName = "";
+
+  while (true) {
+    let promptName = prompt("Enter Your Name ");
+    if (!promptName) {
+      break;
+    }
+    myName = myName + " " + promptName;
+  }
+  console.log(myName);
+}
+*/
+
+// prompt with break with number
+{
+  let sum = 0;
+
+  while (true) {
+    let pNumber = +prompt("Enter Your Number");
+    if (!pNumber) {
+      break;
+    }
+    sum += pNumber;
+  }
+  console.log(sum);
+}
